d16f7cb27a4853055724ee8aa571dfd892459fcb
tokomaramuki1234 <genspark_dev@genspark.ai>
2025-10-27 13:04:59 +0000

    ナビゲーションから「研修について」リンクを削除

--- src/components/Training.js
+++ src/components/Training.js
@@ -116,7 +116,6 @@ const Training = () => {
             </a>
           </div>
           <nav className={`ke-nav ${isMobileMenuOpen ? 'ke-nav-open' : ''}`}>
-            <a href="#problems" onClick={() => setIsMobileMenuOpen(false)}>研修について</a>
             <a href="#features" onClick={() => setIsMobileMenuOpen(false)}>特徴</a>
             <a href="#program" onClick={() => setIsMobileMenuOpen(false)}>プログラム</a>
             <a href="#pricing" onClick={() => setIsMobileMenuOpen(false)}>料金</a>
